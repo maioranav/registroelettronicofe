30067286d571f4f1aaed03d96a3642446f77dd14
Vincenzo M <vincenzomaiorana@gmail.com>
2023-05-10 14:33:25 +0200

    import bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.scss";
+import { Container } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <CustHeader />
         <Container className="my-5">
           <Routes>
             <Route path="/" element={<Login />} />
             <Route path="/wishlist" element={<WishList />} />
-            <Route path="/contactme" element={<ContactMe />} />
-            <Route path="/feed" element={<Feed />} />
+            <Route path="/contattaci" element={<Contattaci />} />
             <Route path="/logout" element={<Logout />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </Container>
-        <CustFooter />
       </BrowserRouter>
     </div>
   );
