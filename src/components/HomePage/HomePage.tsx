b3970f04ce0f6764e8d91b829ab357f6bc639290
Vincenzo M <vincenzomaiorana@gmail.com>
2023-05-10 23:25:01 +0200

    created homepage with img lazyload

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,14 +1,34 @@
 import { Button, Col, Row } from "react-bootstrap";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
 import "./HomePage.scss";
+import { useState } from "react";
 
 export const HomePage = () => {
+  const [shownImage, setShownImage] = useState(false);
+
   return (
     <>
-      <Row>
-        <Col xs={12} lg={6} className="leftColumn"></Col>
-        <Col xs={12} lg={6} className="d-flex p-4 justify-content-center rightColumn flex-column align-items-center align-items-md-start">
+      <Row className="homePageRow">
+        <Col xs={12} lg={6} className="leftColumn">
+          <div>
+            <LazyLoadImage
+              src="http://p-hold.com/castle/800/800"
+              alt="Random Pic"
+              effect="blur"
+              width={"800"}
+              height={"800"}
+              afterLoad={() => {
+                setShownImage(true);
+              }}
+              className="rounded-circle homeRandomImg"
+            ></LazyLoadImage>
+          </div>
+          {shownImage && <h6 className="text-info text-center">Random Picture from p-Hold</h6>}
+        </Col>
+        <Col xs={12} lg={6} className="d-flex p-5 justify-content-center rightColumn flex-column align-items-center align-items-md-start">
           <h1>UniVincenzo.it</h1>
-          <h3 className="d-none d-md-block">Dove Innovazione e Tecnologia, rappresentano il futuro!</h3>
+          <h3 className="d-none d-md-block w-75">Dove Innovazione e Tecnologia, rappresentano il futuro!</h3>
           <Button className="mt-4 rounded-4">Log In</Button>
         </Col>
       </Row>
