dc7d91dee03ca6096cb36803a29108917e253bd2
Vincenzo M <vincenzomaiorana@gmail.com>
2023-05-10 16:36:18 +0200

    404 custom page

---
+++ src/components/NotFound/NotFound.tsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+import "./NotFound.scss";
+import useDocumentTitle from "../../app/useDocumentTitle";
+export const NotFound = () => {
+  useDocumentTitle("Error Page");
+  return (
+    <div className="page-wrap d-flex flex-row align-items-center">
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-md-12 text-center">
+            <span className="display-1 d-block">404</span>
+            <div className="mb-4 lead">The page you are looking for was not found.</div>
+            <Link to="/" className="btn btn-link">
+              Back to Home
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
