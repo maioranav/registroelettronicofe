1a9b340889b72a9f07ebf96f7ab8df1f9a30237f
Vincenzo M <vincenzomaiorana@gmail.com>
2023-05-16 00:07:37 +0200

    add APPELLI dash studente

--- src/components/Dashboard/DashMsgs/DashMsgs.tsx
+++ src/components/Dashboard/DashMsgs/DashMsgs.tsx
@@ -1,11 +1,11 @@
 import "./DashMsgs.scss";
 import { IoIosArrowForward } from "react-icons/io";
 import { HiBellAlert } from "react-icons/hi2";
-import { FcInvite } from "react-icons/fc";
 import { BsPlusCircleFill } from "react-icons/bs";
 import { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../../../app/hooks";
 import { messagesFetch } from "../../../app/reducers/messageSlice";
+import { Col, Container, Row } from "react-bootstrap";
 
 interface IDashProps {
   variante: string;
@@ -18,14 +18,22 @@ export const DashMsgs = ({ variante }: IDashProps) => {
   const msgs = useAppSelector((state) => state.msgs.messages?.content);
 
   useEffect(() => {
-    dispatch(messagesFetch({ accessToken: loginToken, elNo: 4, sort: "data,desc" }));
+    if (variante === "studente") {
+      dispatch(messagesFetch({ accessToken: loginToken, elNo: 2, sort: "data,desc" }));
+    } else {
+      dispatch(messagesFetch({ accessToken: loginToken, elNo: 4, sort: "data,desc" }));
+    }
     //qualcosaqui
   }, [myProfile]);
 
   return (
     <div className="circolariCorsiMobile">
       <h5>
-        Circolari dei tuoi corsi
+        <div className="d-flex align-items-center">
+          {" "}
+          <img src="./icons/note.svg" alt="Circolari" style={{ marginRight: "10px" }} />
+          Circolari dei tuoi corsi
+        </div>
         <span className="text-primary">
           {variante === "studente" && <HiBellAlert />}
           {variante === "docente" && <BsPlusCircleFill />}
@@ -37,7 +45,7 @@ export const DashMsgs = ({ variante }: IDashProps) => {
           msgs.map((el) => (
             <li key={el.id} className="mb-3">
               <div className="d-flex justify-content-center">
-                <FcInvite className="iconaMessaggio" />
+                <img src="./icons/messaggio.svg" alt="Messaggio" className="iconaMessaggio" />
               </div>
               <div>
                 <p>{el.corso.name}</p>
@@ -47,6 +55,30 @@ export const DashMsgs = ({ variante }: IDashProps) => {
             </li>
           ))}
       </ul>
+      {variante === "studente" && (
+        <>
+          <h5>
+            <div className="d-flex align-items-center">
+              <img src="./icons/cappello.svg" alt="Circolari" style={{ marginRight: "10px" }} />
+              Prossimi Appelli
+            </div>
+          </h5>
+          <Row className="d-flex justify-content-between justify-content-md-start appelli">
+            <div className="d-flex flex-column justify-content-center align-items-center">
+              <Col xs={6} className="appelliDiv">
+                <img src="./icons/esami.svg" alt="Appelli d'esame" />
+              </Col>
+              <p className="textAppelli">Appelli d'esame</p>
+            </div>
+            <div className="d-flex flex-column justify-content-center align-items-center">
+              <Col xs={6} className="appelliDiv" style={{ backgroundColor: "#fed9ab" }}>
+                <img src="./icons/laurea.svg" alt="Appelli d'esame" />
+              </Col>
+              <p className="textAppelli">Appelli di Laurea</p>
+            </div>
+          </Row>
+        </>
+      )}
     </div>
   );
 };
