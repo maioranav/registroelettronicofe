2b50c21ee2fc390623d977fdc02f2edd34359aaf
Vincenzo M <vincenzomaiorana@gmail.com>
2023-05-15 22:28:23 +0200

    lezioni fix responsive show

--- src/components/Dashboard/DashCalendar/DashCalendar.tsx
+++ src/components/Dashboard/DashCalendar/DashCalendar.tsx
@@ -1,32 +1,44 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useAppDispatch, useAppSelector } from "../../../app/hooks";
 import "./DashCalendar.scss";
 import { IoIosArrowForward } from "react-icons/io";
 import { lezioniFetch } from "../../../app/reducers/lezioniSlice";
-import { Corso } from "../../../app/custominterfaces";
 import { format } from "date-fns";
 export const DashCalendar = () => {
   const corsi = useAppSelector((state) => state.myProfile?.myProfile?.corsi);
   const loginToken = useAppSelector((state) => state.profile?.token);
   const lezioni = useAppSelector((state) => state.lezioni);
   const dispatch = useAppDispatch();
+  let width = window.innerWidth;
+  const [maxShow, setMaxShow] = useState(2);
 
   useEffect(() => {
     dispatch(lezioniFetch({ accessToken: loginToken.accessToken, corsi: corsi, data: format(new Date(), "yyyy-MM-dd") }));
   }, []);
 
+  useEffect(() => {
+    if (width > 768) {
+      setMaxShow(4);
+    }
+    if (width > 1200) {
+      setMaxShow(5);
+    }
+  }, [width]);
+
   return (
     <div className="calendarioDashMobile my-4">
       <div className="nextLessons">
         {lezioni.status === "idle" &&
-          lezioni.lezioni.length > 0 &&
-          lezioni.lezioni.map((el) => (
-            <div className="mobileCalendarNextLesson">
-              <p className="m-0 text-danger">{el.data + ""}</p>
-              <p className="m-0 text-primary">9:00</p>
-              <p className="m-0">{el.corso.name}</p>
-            </div>
-          ))}
+          lezioni.lezioni?.length > 0 &&
+          lezioni.lezioni
+            .filter((item, index) => index < maxShow)
+            .map((el) => (
+              <div key={el.id} className="mobileCalendarNextLesson">
+                <p className="m-0 text-danger">{el.data + ""}</p>
+                <p className="m-0 text-primary">{el.orario}:00</p>
+                <p className="m-0">{el.corso.name}</p>
+              </div>
+            ))}
       </div>
       <h6 className="text-primary mt-2">
         Vedi tutte le lezioni <IoIosArrowForward />
