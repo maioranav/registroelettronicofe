2abd2a673283dfc89ac827f169d0e17bca4a8289
Vincenzo M <vincenzomaiorana@gmail.com>
2023-05-15 16:08:56 +0200

    exported custom interfaces

---
+++ src/app/custominterfaces.ts
@@ -0,0 +1,42 @@
+export interface PageableFetch<T> {
+  content: T;
+  pageable: Pageable;
+  last: boolean;
+  totalElements: number;
+  totalPages: number;
+  size: number;
+  number: number;
+  sort: Sort;
+  first: boolean;
+  numberOfElements: number;
+  empty: boolean;
+}
+
+export interface Pageable {
+  sort: Sort;
+  offset: number;
+  pageSize: number;
+  pageNumber: number;
+  paged: boolean;
+  unpaged: boolean;
+}
+
+export interface Sort {
+  empty: boolean;
+  sorted: boolean;
+  unsorted: boolean;
+}
+
+export interface Corso {
+  id: number;
+  name: string;
+  docente: Docente;
+}
+
+export interface Docente {
+  id: number;
+  name: string;
+  surname: string;
+  username: string;
+  email: string;
+}
