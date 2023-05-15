cbb0ef96c704e555ddba04317ea9d3c03f163880
Vincenzo M <vincenzomaiorana@gmail.com>
2023-05-15 16:09:44 +0200

    go home if no profile saved dashboard

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -3,12 +3,24 @@ import "./Dashboard.scss";
 import { Profile } from "../Profile/Profile";
 import { DashCalendar } from "./DashCalendar/DashCalendar";
 import { DashMsgs } from "./DashMsgs/DashMsgs";
+import { useAppSelector } from "../../app/hooks";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 interface IDashProps {
   variante: string;
 }
 
 export const Dashboard = ({ variante }: IDashProps) => {
+  const myProfile = useAppSelector((state) => state.myProfile);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!myProfile.myProfile?.id) {
+      navigate("/");
+    }
+  }, []);
+
   return (
     <>
       <Row>
