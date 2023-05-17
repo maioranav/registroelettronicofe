26dfa1fdb8bec935d34ce991716ae14600d2dde7
Vincenzo M <vincenzomaiorana@gmail.com>
2023-05-17 12:14:04 +0200

    page corso list

--- src/components/Corsi/Corsi.tsx
+++ src/components/Corsi/Corsi.tsx
@@ -6,11 +6,34 @@ import { corsiFetch } from "../../app/reducers/corsiSlice";
 export const Corsi = () => {
   const dispatch = useAppDispatch();
   const loginToken = useAppSelector((state) => state.profile?.token);
+  const corsi = useAppSelector((state) => state.corsi);
   const [page, setPage] = useState(0);
 
   useEffect(() => {
     dispatch(corsiFetch({ accessToken: loginToken.accessToken, elNo: 5, page: page }));
   }, []);
+
+  useEffect(() => {
+    if (page !== corsi.corsi?.pageable?.pageNumber) {
+      setPage(corsi.corsi?.pageable?.pageNumber);
+    }
+  }, [corsi.corsi?.pageable?.pageNumber]);
+
+  useEffect(() => {
+    dispatch(corsiFetch({ accessToken: loginToken.accessToken, elNo: 5, page: page }));
+  }, [page]);
+
+  const handlePage = (action: string) => {
+    if (action === "prev") {
+      if (page - 1 > -1) {
+        setPage(page - 1);
+      }
+    } else if (action === "next") {
+      if (page + 1 < corsi.corsi?.totalPages) {
+        setPage(page + 1);
+      }
+    }
+  };
 
   return (
     <Col xs={12} md={9} lg={10}>
@@ -23,19 +46,20 @@ export const Corsi = () => {
         <Row>
           <div className="listContainer">
             <ul className="p-0">
-              {
-                <li className="d-flex justify-content-between py-2 my-2 align-items-center">
-                  <div>
-                    <p className="listData">Data</p>
-                    <p className="listTitle">Titolo - Nome Docente</p>
-                  </div>
-                  <div className="listIcone">
-                    <img src="../../../icons/modifica.svg" alt="Modifica" />
-                    <img src="../../../icons/listpresenze.svg" alt="Lista Presenze" />
-                    <img src="../../../icons/delete.svg" alt="Elimina" />
-                  </div>
-                </li>
-              }
+              {corsi.corsi?.content?.length > 0 &&
+                corsi.corsi.content.map((el) => (
+                  <li key={el.id} className="d-flex justify-content-between py-2 my-2 align-items-center">
+                    <div>
+                      <p className="listData">ID Corso: {el.id}</p>
+                      <p className="listTitle">{el.name}</p>
+                    </div>
+                    <div className="listIcone">
+                      <img src="../../../icons/modifica.svg" alt="Modifica" />
+                      <img src="../../../icons/listpresenze.svg" alt="Lista Presenze" />
+                      <img src="../../../icons/delete.svg" alt="Elimina" />
+                    </div>
+                  </li>
+                ))}
             </ul>
             <Button className="listButtonNew">
               <img src="../../../icons/plus.svg" alt="Aggiungi" />
@@ -43,13 +67,29 @@ export const Corsi = () => {
             </Button>
             <div className="my-4 d-flex justify-content-center" style={{ border: "none" }}>
               <Pagination>
-                <Pagination.First />
-                <Pagination.Prev />
+                <Pagination.First
+                  onClick={() => {
+                    setPage(0);
+                  }}
+                />
+                <Pagination.Prev
+                  onClick={() => {
+                    handlePage("prev");
+                  }}
+                />
                 <Pagination.Ellipsis disabled />
-                <Pagination.Item active>{12}</Pagination.Item>
+                <Pagination.Item active>{page + 1}</Pagination.Item>
                 <Pagination.Ellipsis disabled />
-                <Pagination.Next />
-                <Pagination.Last />
+                <Pagination.Next
+                  onClick={() => {
+                    handlePage("next");
+                  }}
+                />
+                <Pagination.Last
+                  onClick={() => {
+                    setPage(corsi.corsi?.totalPages - 1);
+                  }}
+                />
               </Pagination>
             </div>
           </div>
