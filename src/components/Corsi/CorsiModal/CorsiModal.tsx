c08f573a99a1268913b011369943241d72085c15
Vincenzo M <vincenzomaiorana@gmail.com>
2023-05-22 15:03:46 +0200

    fix save corso-docnte

--- src/components/Corsi/CorsiModal/CorsiModal.tsx
+++ src/components/Corsi/CorsiModal/CorsiModal.tsx
@@ -1,7 +1,7 @@
 import { Button, Modal } from "react-bootstrap";
 import "./CorsiModal.scss";
 import { FormEvent, useEffect, useState } from "react";
-import { Corso } from "../../../app/custominterfaces";
+import { Corso, Docente } from "../../../app/custominterfaces";
 import { useAppSelector } from "../../../app/hooks";
 import { Form } from "react-router-dom";
 
@@ -20,6 +20,8 @@ interface fetchParam {
 
 export const CorsiModal = ({ show, handleClose, id }: CorsiModalProps) => {
   const loginToken = useAppSelector((state) => state.profile);
+  const [allDocenti, setAllDocenti] = useState<Docente[]>();
+  const [docente, setDocente] = useState<Docente>();
   const [dataState, setDataState] = useState({ name: "" } as Corso);
 
   const fetchCorso = async ({ id, method = "GET", body }: fetchParam) => {
@@ -58,19 +60,44 @@ export const CorsiModal = ({ show, handleClose, id }: CorsiModalProps) => {
     }
   };
 
+  const fetchAllDocenti = async () => {
+    try {
+      let response = await fetch(process.env.REACT_APP_APIURL + "/docenti/all", {
+        method: "GET",
+        headers: { Authorization: "Bearer " + loginToken.token?.accessToken },
+      });
+      if (response.ok) {
+        const data: Docente[] = await response.json();
+        setAllDocenti(data);
+        return data;
+      } else {
+        const res = await response.json();
+        console.log(res.message);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
     if (dataState.id != null) {
-      fetchCorso({ method: "PUT", body: { id: dataState.id, name: dataState.name, plesso: { id: 1 } } });
+      fetchCorso({ method: "PUT", body: { id: dataState.id, name: dataState.name, plesso: { id: 1 }, docente: docente } });
     } else {
-      fetchCorso({ method: "POST", body: { name: dataState.name, plesso: { id: 1 } } });
+      fetchCorso({ method: "POST", body: { name: dataState.name, plesso: { id: 1 }, docente: docente } });
     }
+  };
+
+  const handleDocente = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    let selezionato = allDocenti?.find((el) => el.id + "" === e.target.value);
+    setDocente(selezionato);
   };
 
   useEffect(() => {
     if (id != null) {
       fetchCorso({ id });
     }
+    fetchAllDocenti();
   }, [id]);
 
   return (
@@ -82,16 +109,29 @@ export const CorsiModal = ({ show, handleClose, id }: CorsiModalProps) => {
             {id !== null && <Modal.Title>Modifica Corso</Modal.Title>}
           </Modal.Header>
           <Modal.Body>
-            <label htmlFor="name">Nome Corso:</label>
-            <input
-              type="text"
-              id="name"
-              placeholder="Nome Corso"
-              value={dataState.name}
-              onChange={(e) => {
-                setDataState({ ...dataState, name: e.target.value });
-              }}
-            />
+            <div>
+              <label htmlFor="name">Nome Corso:</label>
+              <input
+                type="text"
+                id="name"
+                placeholder="Nome Corso"
+                value={dataState.name}
+                onChange={(e) => {
+                  setDataState({ ...dataState, name: e.target.value });
+                }}
+              />
+            </div>
+            <div>
+              <label htmlFor="docente">Docente:</label>
+              <select id="docente" onChange={handleDocente}>
+                <option value={"null"}>Seleziona docente...</option>
+                {allDocenti?.map((el) => (
+                  <option key={"doc" + el.id} value={el.id}>
+                    {el.name + " " + el.surname}
+                  </option>
+                ))}
+              </select>
+            </div>
           </Modal.Body>
           <Modal.Footer>
             <Button variant="secondary" onClick={handleClose}>
